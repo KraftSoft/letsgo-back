a39d500bb54e0e23da9e610b3d706eae2d8080ec
Ilia Kravtsov <idakraftsoft@gmail.com>
2017-02-11 10:39:10 +0300

    fix urls regexp

--- core/urls.py
+++ core/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     url(r'^user-create/$', UserCreate.as_view(), name='user-create'),
     url(r'^users-list/$', UserList.as_view(), name='users-list'),
 
-    url(r'^user-detail$', UserDetail.as_view(), name='user-detail'),
-    url(r'^user-detail/(?P<pk>\d+)$', UserDetail.as_view(), name='user-detail'),
+    url(r'^user-detail/$', UserDetail.as_view(), name='user-detail'),
+    url(r'^user-detail/(?P<pk>\d+)/$', UserDetail.as_view(), name='user-detail'),
 
     url(r'^meetings-list/$', MeetingsList.as_view(), name='meetings-list'),
 
-    url(r'^meeting-detail$', MeetingDetail.as_view(), name='meeting-detail'),
+    url(r'^meeting-detail/$', MeetingDetail.as_view(), name='meeting-detail'),
     url(r'^meeting-detail/(?P<pk>\d+)/$', MeetingDetail.as_view(), name='meeting-detail'),
 ]
