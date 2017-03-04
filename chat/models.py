067e087bfcecde4c3cff27c192d7132dcde27448
kic <kravcov2512@gmail.com>
2017-03-04 13:51:22 +0300

    add migrations

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,17 @@ from core.models import User
 from django.db import models
 
 
+class Confirm(models.Model):
+    meeting = models.ForeignKey('core.Meeting', related_name='confirms')
+    user = models.ForeignKey('core.User', related_name='confirms')
+
+    date_create = models.DateTimeField(auto_now_add=True)
+    last_modify = models.DateTimeField(auto_now=True)
+
+    is_approved = models.BooleanField(default=False)
+    is_rejected = models.BooleanField(default=False)
+
+
 class Chat(models.Model):
     title = models.CharField(max_length=32)
     users = models.ManyToManyField(User)
